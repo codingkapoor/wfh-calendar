97b85d375923e339a0dfaa85df085a9c207fc93d
codingkapoor <mailtoshivamk@gmail.com>
2017-08-13 14:45:40 +0530

    half/full day star selection

--- js/main.js
+++ js/main.js
@@ -4,30 +4,48 @@
 $(document).ready(function () {
     'use strict';
 
-    $('i').on('click', function (e) {
-        var iWidth = $(this).outerWidth();
-        var iOffset = $(this).offset();
+    var toggleStatus = true;
 
-        var x = e.pageX - iOffset.left;
-        if (iWidth / 2 > x) {
-            if ($(this).hasClass('fa-star-o')) {
-                $(this).toggleClass('fa-star-o fa-star-half-o');
-            } else if ($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
-                $(this).toggleClass('fa-star-half-o fa-flip-horizontal fa-star');
-            } else if ($(this).hasClass('fa-star-half-o')) {
-                $(this).toggleClass('fa-star-half-o fa-star-o');
-            } else if ($(this).hasClass('fa-star')) {
-                $(this).toggleClass('fa-star fa-star-half-o fa-flip-horizontal');
+    $('#selection').change(function () {
+        if ($(this).prop('checked')) {
+            toggleStatus = true;
+        } else {
+            toggleStatus = false;
+        }
+    })
+
+    $('i').on('click', function (e) {
+        if (toggleStatus) {
+            if($(this).hasClass('fa-star')) {
+                $(this).toggleClass('fa-star fa-star-o');
+            } else {
+                $(this).removeClass('fa-star-o fa-star-half-o fa-flip-horizontal').addClass('fa-star');
             }
         } else {
-            if ($(this).hasClass('fa-star-o')) {
-                $(this).toggleClass('fa-star-o fa-star-half-o fa-flip-horizontal');
-            } else if ($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
-                $(this).toggleClass('fa-star-half-o fa-flip-horizontal fa-star-o');
-            } else if ($(this).hasClass('fa-star-half-o')) {
-                $(this).toggleClass('fa-star-half-o fa-star');
-            } else if ($(this).hasClass('fa-star')) {
-                $(this).toggleClass('fa-star fa-star-half-o');
+            var iWidth = $(this).outerWidth();
+            var iOffset = $(this).offset();
+
+            var x = e.pageX - iOffset.left;
+            if (iWidth / 2 > x) {
+                if ($(this).hasClass('fa-star-o')) {
+                    $(this).toggleClass('fa-star-o fa-star-half-o');
+                } else if ($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
+                    $(this).toggleClass('fa-star-half-o fa-flip-horizontal fa-star');
+                } else if ($(this).hasClass('fa-star-half-o')) {
+                    $(this).toggleClass('fa-star-half-o fa-star-o');
+                } else if ($(this).hasClass('fa-star')) {
+                    $(this).toggleClass('fa-star fa-star-half-o fa-flip-horizontal');
+                }
+            } else {
+                if ($(this).hasClass('fa-star-o')) {
+                    $(this).toggleClass('fa-star-o fa-star-half-o fa-flip-horizontal');
+                } else if ($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
+                    $(this).toggleClass('fa-star-half-o fa-flip-horizontal fa-star-o');
+                } else if ($(this).hasClass('fa-star-half-o')) {
+                    $(this).toggleClass('fa-star-half-o fa-star');
+                } else if ($(this).hasClass('fa-star')) {
+                    $(this).toggleClass('fa-star fa-star-half-o');
+                }
             }
         }
     });
