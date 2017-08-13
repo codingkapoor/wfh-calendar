60d044d4101fd90fc8f7f97da9be77cc0aa3f191
codingkapoor <mailtoshivamk@gmail.com>
2017-08-13 10:36:58 +0530

    all icon are made selectable

--- js/main.js
+++ js/main.js
@@ -1,40 +1,35 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 
-$(document).ready(function() {
+$(document).ready(function () {
     'use strict';
-    
-    $("#x20").click(function (e) {
+
+    $('i').on('click', function (e) {
         var iWidth = $(this).outerWidth();
         var iOffset = $(this).offset();
 
         var x = e.pageX - iOffset.left;
         if (iWidth / 2 > x) {
-            if($(this).hasClass('fa-star-o')) {
+            if ($(this).hasClass('fa-star-o')) {
                 $(this).toggleClass('fa-star-o fa-star-half-o');
-            }
-            else if($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
+            } else if ($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
                 $(this).toggleClass('fa-star-half-o fa-flip-horizontal fa-star');
-            }
-            else if($(this).hasClass('fa-star-half-o')) {
+            } else if ($(this).hasClass('fa-star-half-o')) {
                 $(this).toggleClass('fa-star-half-o fa-star-o');
-            }
-            else if($(this).hasClass('fa-star')) {
+            } else if ($(this).hasClass('fa-star')) {
                 $(this).toggleClass('fa-star fa-star-half-o fa-flip-horizontal');
             }
         } else {
-            if($(this).hasClass('fa-star-o')) {
+            if ($(this).hasClass('fa-star-o')) {
                 $(this).toggleClass('fa-star-o fa-star-half-o fa-flip-horizontal');
-            }
-            else if($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
+            } else if ($(this).hasClass('fa-star-half-o fa-flip-horizontal')) {
                 $(this).toggleClass('fa-star-half-o fa-flip-horizontal fa-star-o');
-            }
-            else if($(this).hasClass('fa-star-half-o')) {
+            } else if ($(this).hasClass('fa-star-half-o')) {
                 $(this).toggleClass('fa-star-half-o fa-star');
-            }
-            else if($(this).hasClass('fa-star')) {
+            } else if ($(this).hasClass('fa-star')) {
                 $(this).toggleClass('fa-star fa-star-half-o');
             }
         }
     });
+
 });
